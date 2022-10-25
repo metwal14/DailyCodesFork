327e7b9d36cc0cd92e717afaea1ee11eb4866ccf
Ankit Metwal <ankitmetwal10jan@gmail.com>
2022-10-25 14:53:35 -0400

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -8,7 +8,7 @@ for i in range(1, n+1):
          if i%15 == 0:
                   print("first" , end = " ")
          elif i%3 == 0:
-                  print("fizz", end = " ")
+                  print("second", end = " ")
          elif i%5 == 0:
                   print("buzz", end = " ")
          else:
